93634887a82b2215ea1f4fe60c707e04f6c19d9a
takahashi <highbridge3030@gmail.com>
2023-11-11 20:41:26 +0900

    Cloudinaryのインスタンス化を追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+// 306-44
+use App\Modules\ImageUpload\CloudinaryImageManager;
+use App\Modules\ImageUpload\ImageManagerInterface;
+use App\Modules\ImageUpload\LocalImageManager;
+// 301-37
+use Cloudinary\Cloudinary;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // 301-37
+        $this->app->bind(Cloundinary::class, function () {
+            return new Cloudinary([
+                'clound' => [
+                    'cloud_name' => config('cloudinary.cloud_name'),
+                    'api_key'    => config('cloudinary.api_key'),
+                    'api_secret' => config('cloudinary.api_secret'),
+                ],
+            ]);
+        });
+        // 307-44
+        if ($this->app->environment('production')) {
+            $this->app->bind(ImageManagerInterface::class, CloudinaryImageManager::class);
+        } else {
+            $this->app->bind(ImageManagerInterface::class, LocalImageManager::class);
+        }
     }
 
     /**
